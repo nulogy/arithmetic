99c59a31c08d8d75e297fb43e1d5b38f880e4599
Sean Kirby <seank@nulogy.com>
2012-10-25 18:48:48 -0400

    lazily evaluates the result of an expression

--- lib/arithmetic/expression.rb
+++ lib/arithmetic/expression.rb
@@ -1,17 +1,18 @@
-class Arithmetic::Expression
-  attr_reader :original_expression
+module Arithmetic
+  class Expression
+    attr_reader :original_expression
 
-  def initialize(expression)
-    @original_expression = expression
-    @parsed_expression = Parser.new(expression).parse
-    @result = @parsed_expression.eval
-  end
+    def initialize(expression)
+      @original_expression = expression
+      @parsed_expression = Parser.new(expression).parse
+    end
 
-  def eval
-    @result
-  end
+    def eval
+      @result ||= @parsed_expression.eval
+    end
 
-  def to_s
-    @parsed_expression.to_s(:infix)
+    def to_s
+      @parsed_expression.to_s(:infix)
+    end
   end
 end

be3f3dcb94d11398c558d10e5e69832ec58ba32e
Sean Kirby <seank@nulogy.com>
2012-10-25 21:48:34 -0400

    moves nodes into their own file

--- lib/arithmetic.rb
+++ lib/arithmetic.rb
@@ -1,8 +1,10 @@
 require 'bigdecimal'
 require 'arithmetic/expression'
 require 'arithmetic/parser'
+require 'arithmetic/nodes'
 
 module Arithmetic
+  # make lazy?
   def self.parse(expression)
     Expression.new(Parser.new(expression).parse)
   end
--- lib/arithmetic/parser.rb
+++ lib/arithmetic/parser.rb
@@ -137,47 +137,6 @@ module Arithmetic
       @operators[token]
     end
   end
-   
-  class OperandNode
-    attr_accessor :operand
-   
-    def initialize(operand)
-      @operand = operand
-    end
-
-    def to_s(na=nil)
-      @operand
-    end
-   
-    def eval
-      BigDecimal.new(@operand)
-    end
-  end
-
-  class OperatorNode
-    attr_accessor :operator, :operands
-   
-    def initialize(operator, operands)
-      @operator = operator
-      @operands = operands
-    end
-   
-    def to_s(top=true)
-      strs = @operands.map {|o| o.to_s(false) }
-
-      if @operator.arity == 1
-        "#{@operator}#{strs.first}"
-      else
-        result = [strs.first, @operator, *strs[1..-1]].join(" ")
-        result = "(" + result + ")" unless top
-        result
-      end
-    end
-   
-    def eval
-      @operator.eval(*@operands.map(&:eval))
-    end
-  end
 
   class InvalidExpression < Exception
   end

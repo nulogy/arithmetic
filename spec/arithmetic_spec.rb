fa1aab52834a60b83889af4a91f8aafb0d809949
Jason Schweier <jasons@nulogy.com>
2020-02-27 17:54:25 -0500

    Update to RSpec expect syntax

--- spec/arithmetic_spec.rb
+++ spec/arithmetic_spec.rb
@@ -2,47 +2,47 @@ require 'spec_helper'
 
 describe Arithmetic do
   it "evaluates simple expressions" do
-    test_eval("2").should == 2.0
+    expect(test_eval("2")).to be == 2.0
   end
 
   it "evaluates addition" do
-    test_eval("2 + 2").should == 4.0
+    expect(test_eval("2 + 2")).to be == 4.0
   end
 
   it "evaluates subtraction" do
-    test_eval("2.1 - 2").should == 0.1
+    expect(test_eval("2.1 - 2")).to be == 0.1
   end
 
   it "handles negative numbers" do
-    test_eval("3--2").should == 5
+    expect(test_eval("3--2")).to be == 5
   end
 
   it "handles negative numbers with parens" do
-    test_eval("-(3+2)").should == -5
+    expect(test_eval("-(3+2)")).to be == -5
   end
 
   it "handles leading minus signs" do
-    test_eval("-3+2").should == -1
+    expect(test_eval("-3+2")).to be == -1
   end
 
   it "has unary minus take precedence over multiplication" do
-    test_eval("-3 * -2").should == 6
+    expect(test_eval("-3 * -2")).to be == 6
   end
 
   it "evaluates division" do
-    test_eval("10.5 / 5").should == 2.1
+    expect(test_eval("10.5 / 5")).to be == 2.1
   end
 
   it "evaluates multiplication" do
-    test_eval("2 * 3.1").should == 6.2
+    expect(test_eval("2 * 3.1")).to be == 6.2
   end
 
   it "evaluates parens" do
-    test_eval("2 * (2.1 + 1)").should == 6.2
+    expect(test_eval("2 * (2.1 + 1)")).to be == 6.2
   end
 
   it "evaluates regardless of whitespace" do
-    test_eval("2*(1+\t1)").should == 4
+    expect(test_eval("2*(1+\t1)")).to be == 4
   end
 
   it "evaluates order of operations" do
@@ -50,27 +50,27 @@ describe Arithmetic do
   end
 
   it "evaluates multiple levels of parens" do
-    test_eval("2*(1/(1+3))").should == 0.5
+    expect(test_eval("2*(1/(1+3))")).to be == 0.5
   end
 
   it "formats the expression" do
-    test_to_s("   -1+\n    2*     \t3").should == '-1 + (2 * 3)'
+    expect(test_to_s("   -1+\n    2*     \t3")).to be == '-1 + (2 * 3)'
   end
 
   it "formats the expression using a custom visitor" do
-    test_to_s("-1 + 2 * 3", ->(n) { "x#{n}x" }).should == 'x-xx1x x+x x(xx2x x*x x3xx)x'
+    expect(test_to_s("-1 + 2 * 3", ->(n) { "x#{n}x" })).to be == 'x-xx1x x+x x(xx2x x*x x3xx)x'
   end
 
   it "handles ridiculous precision" do
-    test_eval("1.111111111111111111111111111111111111111111 + 2").should == BigDecimal('3.111111111111111111111111111111111111111111')
+    expect(test_eval("1.111111111111111111111111111111111111111111 + 2")).to be == BigDecimal('3.111111111111111111111111111111111111111111')
   end
 
   it "handles simple numbers" do
-    test_eval(2).should == 2
+    expect(test_eval(2)).to be == 2
   end
 
   it "handles dangling decimal points" do
-    test_eval("0.").should == 0
+    expect(test_eval("0.")).to be == 0
   end
 
   context "invalid expressions" do
